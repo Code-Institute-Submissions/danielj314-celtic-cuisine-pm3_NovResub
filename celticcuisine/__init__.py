bc9f4cc2fd20b193a863a9ad6b9a60ef3aeb434e
danielj314 <danieljones314@gmail.com>
2022-10-10 22:15:22 +0000

    Connected Flask to MongoDB

--- celticcuisine/__init__.py
+++ celticcuisine/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env  # noqa
 
@@ -8,7 +10,11 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
 
 db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
 from celticcuisine import routes  # noqa
--- celticcuisine/routes.py
+++ celticcuisine/routes.py
@@ -1,8 +1,14 @@
-from flask import render_template
-from celticcuisine import app, db
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
+from celticcuisine import app, db, mongo
 from celticcuisine.models import Nations, Users
 
 
 @app.route("/")
-def home():
-    return render_template("base.html")
+@app.route("/get_recipes")
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
