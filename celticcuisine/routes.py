6d2e9ab433b0402dd5e64d5e570397c691f77ca4
danielj314 <danieljones314@gmail.com>
2022-10-12 00:13:38 +0000

    Added register functionality

--- celticcuisine/routes.py
+++ celticcuisine/routes.py
@@ -15,6 +15,36 @@ def home():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # checks if username already exists in db
+        existing_user = Users.query.filter(Users.user_name == \
+                                           request.form.get("username").lower()).all()
+        
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        # # checks if password and retype password fields match
+        # password_error = request.form.get("password") != \
+        #                   request.form.get("cpassword")
+                                           
+        # if password_error:
+        #     flash("Passwords do not match. Please ensure that both fields are identical.")
+        #     return redirect(url_for("register"))
+        
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+        
+        db.session.add(user)
+        db.session.commit()
+
+        # insert new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
